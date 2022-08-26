b1c59f48a5581edae9446198483a38be2c9565d4
jkica <jki@valuer.ai>
2022-08-26 21:25:42 +0200

    add mobile detection

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMobileDetection from "vue-mobile-detection";
 
 Vue.config.productionTip = false
+
+Vue.use(VueMobileDetection);
 
 new Vue({
   router,
