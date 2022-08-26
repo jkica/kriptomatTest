17568e4a1e4d72e9c349222456d91c5fe5882dba
jkica <jki@valuer.ai>
2022-08-26 21:00:21 +0200

    initial setup

---
+++ src/views/AllCurrencies.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>This is all currencies page</h1>
+    <Card/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Card from '@/components/Card.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Card
+  }
+}
+</script>
