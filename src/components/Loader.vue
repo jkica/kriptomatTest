42e6f32b8543975b38aa14030ed90550bab0bb33
jkica <jki@valuer.ai>
2022-08-28 18:28:59 +0200

    implement loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="loader">
+    <img
+        v-if="show"
+        src="../assets/icons/loader.svg"
+        alt="loader">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loader",
+  props: ['show']
+}
+</script>
+
+<style scoped>
+.loader {
+  position: fixed;
+  top: 45vh;
+  left: 45vw;
+
+}
+</style>
--- src/views/SingleCurrency.vue
+++ src/views/SingleCurrency.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="about">
     <router-link to="/">Go back</router-link>
-    <div v-if="item">
-      <img :src="item.image.small" :alt="`${item.id}-image`">
-      {{item.id}}
-      {{item.symbol.toUpperCase()}}
-      €{{item.market_data.current_price.eur}}
+    <div>
+      <Loader :show="busy"></Loader>
+      <div v-if="item && !busy">
+        <img :src="item.image.small" :alt="`${item.id}-image`">
+        {{item.id}}
+        {{item.symbol.toUpperCase()}}
+        €{{item.market_data.current_price.eur}}
+      </div>
+      <LineChart
+          v-show="!busy"
+          :item="item" />
     </div>
-    <LineChart :item="item" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import LineChart from '@/components/Line.vue'
+import Loader from "@/components/Loader";
 
 export default {
   components: {
+    Loader,
     LineChart
   },
   data () {
     return {
       apiUrl: process.env.VUE_APP_API_URL,
       id: this.$route.params.id,
-      item: null
+      item: null,
+      busy: true
     }
   },
   methods: {
@@ -39,7 +47,8 @@ export default {
         .get(`${this.apiUrl}/coins/${this.id}`)
         .then(res => {
           this.item = res.data;
-          console.log('banana', res.data)
+          // should wait for api response event from chart to set busy to false, not use timeout
+          setTimeout(() => {this.busy  =false}, 500)
         })
   }
 }
